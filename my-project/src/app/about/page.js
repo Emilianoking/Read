e5b87fd4ac95e8b202ab1107f027c607b546a082
Juan Camilo Gómez Duarte <Juncaxd12@gmail.com>
2025-02-10 15:48:18 -0500

    primera clase

---
+++ my-project/src/app/about/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+       <div>
+      <h1>
+        About
+      </h1>
+      <p>Esta es mi primera web</p>
+    </div>
+    </div>
+  )
+}
---
+++ my-project/src/app/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>
+        Hola mundo
+      </h1>
+      <p>Esta es mi primera web</p>
+    </div>
+  );
+}
